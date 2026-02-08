ed8536773c3e863e528d224004339519eb26e0bc
kimsb <key34q0501@gmail.com>
2026-02-08 23:06:31 +0900

    background-photo add

--- drama_data.js
+++ drama_data.js
@@ -1,34 +1,162 @@
 let items = [
-    { text: "날이 좋아서, 날이 좋지 않아서, 날이 적당해서, 모든 날이 좋았다.", title: "도깨비", year: 2016 },
-    { text: "어이가 없네?", title: "베테랑", year: 2015 },
-    { text: "호의가 계속되면, 그게 권리인 줄 알아요.", title: "부당거래", year: 2010 },
-    { text: "이게 최선입니까? 확실해요?", title: "시크릿 가든", year: 2010 },
-    { text: "우리는 답을 찾을 것이다. 늘 그랬듯이.", title: "인터스텔라", year: 2014 },
-    { text: "사과할까요, 고백할까요?", title: "태양의 후예", year: 2016 },
-    { text: "전적으로 저를 믿으셔야 합니다.", title: "SKY 캐슬", year: 2018 },
-    { text: "멋지다 연진아! 브라보!", title: "더 글로리", year: 2022 },
-    { text: "꿈은 도망가지 않아. 도망가는 건 언제나 자신이지.", title: "짱구는 못말려 극장판 9기", year: 2001 },
-    { text: "지금까지 이런 맛은 없었다. 이것은 갈비인가 통닭인가.", title: "극한직업", year: 2019 },
-    { text: "가끔은 잘못 탄 기차가 목적지에 데려다준다.", title: "사랑의 불시착", year: 2019 },
-    { text: "우린 깐부잖아.", title: "오징어 게임", year: 2021 },
-    { text: "내 가치를 네가 정하지 마. 내 인생 이제 시작이니까.", title: "이태원 클라쓰", year: 2020 },
-    { text: "러브가 무엇이오? 총 쏘는 것보다 어렵소?", title: "미스터 션샤인", year: 2018 },
-    { text: "버텨라. 이곳은 전쟁터다. 밖은 지옥이다.", title: "미생", year: 2014 },
-    { text: "아직 신에게는 12척의 배가 남아있사옵니다.", title: "명량", year: 2014 },
-    { text: "포기하지 않으면 희망은 있습니다.", title: "시그널", year: 2016 },
-    { text: "너 내 누군지 아니, 내 하얼빈 장첸이야!", title: "범죄도시", year: 2017 },
-    { text: "기러기, 토마토, 스위스, 인도인, 별똥별, 우영우.", title: "이상한 변호사 우영우", year: 2022 },
-    { text: "국가란 국민입니다.", title: "변호인", year: 2013 },
-    { text: "내 딸 죽인 사람 누구야!", title: "펜트하우스", year: 2020 },
-    { text: "사랑은 타이밍이다.", title: "응답하라 1988", year: 2015 },
-    { text: "시대는 쉽게 네 꿈을 뺏을 수 있어.", title: "스물다섯 스물하나", year: 2022 },
-    { text: "살아있는 사람들은 어떻게든 살아야 한다.", title: "킹덤", year: 2019 },
-    { text: "우리는 모두 시궁창 속에 살고 있지만, 누군가는 별을 바라본다.", title: "나의 아저씨", year: 2018 },
-    { text: "천송이 씨, 내가... 너를...", title: "별에서 온 그대", year: 2013 },
-    { text: "실패하면 반역, 성공하면 혁명 아닙니까!", title: "서울의 봄", year: 2023 },
-    { text: "동작 그만. 밑장 빼기냐?", title: "타짜", year: 2006 },
-    { text: "나 너 좋아하냐?", title: "상속자들", year: 2013 },
-    { text: "죽는 게 무서운 게 아니라, 잊히는 게 무섭다.", title: "슬기로운 의사생활", year: 2020 },
-    { text: "밥 잘 사주는 예쁜 누나.", title: "밥 잘 사주는 예쁜 누나", year: 2018 },
-    { text: "몰랐으니까.. 해방될 지 몰랐으니까!", title: "암살", year: 2015}
+    { 
+        text: "날이 좋아서, 날이 좋지 않아서, 날이 적당해서, 모든 날이 좋았다.", 
+        title: "도깨비", year: 2016,
+        image: "drama_img/goblin.jpg"
+    },
+    { 
+        text: "어이가 없네?", 
+        title: "베테랑", year: 2015,
+        image: "drama_img/veteran.jpg"
+    },
+    { 
+        text: "호의가 계속되면, 그게 권리인 줄 알아요.", 
+        title: "부당거래", year: 2010,
+        image: "drama_img/unjust_transaction.jpg"
+    },
+    { 
+        text: "이게 최선입니까? 확실해요?", 
+        title: "시크릿 가든", year: 2010,
+        image: "drama_img/secret_garden.jpg"
+    },
+    { 
+        text: "우리는 답을 찾을 것이다. 늘 그랬듯이.", 
+        title: "인터스텔라", year: 2014,
+        image: "drama_img/interstellar.jpg"
+    },
+    { 
+        text: "사과할까요, 고백할까요?", 
+        title: "태양의 후예", year: 2016,
+        image: "drama_img/descendants_of_the_sun.jpg"
+    },
+    { 
+        text: "전적으로 저를 믿으셔야 합니다.", 
+        title: "SKY 캐슬", year: 2018,
+        image: "drama_img/sky_castle.jpg"
+    },
+    { 
+        text: "멋지다 연진아! 브라보!", 
+        title: "더 글로리", year: 2022,
+        image: "drama_img/the_glory.jpg"
+    },
+    { 
+        text: "꿈은 도망가지 않아. 도망가는 건 언제나 자신이지.", 
+        title: "짱구는 못말려 극장판 9기", year: 2001,
+        image: "drama_img/shinchan_movie_9.jpg"
+    },
+    { 
+        text: "지금까지 이런 맛은 없었다. 이것은 갈비인가 통닭인가.", 
+        title: "극한직업", year: 2019,
+        image: "drama_img/extreme_job.jpg"
+    },
+    { 
+        text: "가끔은 잘못 탄 기차가 목적지에 데려다준다.", 
+        title: "사랑의 불시착", year: 2019,
+        image: "drama_img/crash_landing_on_you.jpg"
+    },
+    { 
+        text: "우린 깐부잖아.", 
+        title: "오징어 게임", year: 2021,
+        image: "drama_img/squid_game.jpg"
+    },
+    { 
+        text: "내 가치를 네가 정하지 마. 내 인생 이제 시작이니까.", 
+        title: "이태원 클라쓰", year: 2020,
+        image: "drama_img/itaewon_class.jpg"
+    },
+    { 
+        text: "러브가 무엇이오? 총 쏘는 것보다 어렵소?", 
+        title: "미스터 션샤인", year: 2018,
+        image: "drama_img/mr_sunshine.jpg"
+    },
+    { 
+        text: "버텨라. 이곳은 전쟁터다. 밖은 지옥이다.", 
+        title: "미생", year: 2014,
+        image: "drama_img/misaeng.jpg"
+    },
+    { 
+        text: "아직 신에게는 12척의 배가 남아있사옵니다.", 
+        title: "명량", year: 2014,
+        image: "drama_img/roaring_currents.jpg"
+    },
+    { 
+        text: "포기하지 않으면 희망은 있습니다.", 
+        title: "시그널", year: 2016,
+        image: "drama_img/signal.jpg"
+    },
+    { 
+        text: "너 내 누군지 아니, 내 하얼빈 장첸이야!", 
+        title: "범죄도시", year: 2017,
+        image: "drama_img/the_outlaws.jpg"
+    },
+    { 
+        text: "기러기, 토마토, 스위스, 인도인, 별똥별, 우영우.", 
+        title: "이상한 변호사 우영우", year: 2022,
+        image: "drama_img/attorney_woo.jpg"
+    },
+    { 
+        text: "국가란 국민입니다.", 
+        title: "변호인", year: 2013,
+        image: "drama_img/the_attorney.jpg"
+    },
+    { 
+        text: "내 딸 죽인 사람 누구야!", 
+        title: "펜트하우스", year: 2020,
+        image: "drama_img/penthouse.jpg"
+    },
+    { 
+        text: "사랑은 타이밍이다.", 
+        title: "응답하라 1988", year: 2015,
+        image: "drama_img/reply_1988.jpg"
+    },
+    { 
+        text: "시대는 쉽게 네 꿈을 뺏을 수 있어.", 
+        title: "스물다섯 스물하나", year: 2022,
+        image: "drama_img/twenty_five_twenty_one.jpg"
+    },
+    { 
+        text: "살아있는 사람들은 어떻게든 살아야 한다.", 
+        title: "킹덤", year: 2019,
+        image: "drama_img/kingdom.jpg"
+    },
+    { 
+        text: "우리는 모두 시궁창 속에 살고 있지만, 누군가는 별을 바라본다.", 
+        title: "나의 아저씨", year: 2018,
+        image: "drama_img/my_mister.jpg"
+    },
+    { 
+        text: "천송이 씨, 내가... 너를...", 
+        title: "별에서 온 그대", year: 2013,
+        image: "drama_img/my_love_from_the_star.jpg"
+    },
+    { 
+        text: "실패하면 반역, 성공하면 혁명 아닙니까!", 
+        title: "서울의 봄", year: 2023,
+        image: "drama_img/12_12_the_day.jpg"
+    },
+    { 
+        text: "동작 그만. 밑장 빼기냐?", 
+        title: "타짜", year: 2006,
+        image: "drama_img/tazza.jpg"
+    },
+    { 
+        text: "나 너 좋아하냐?", 
+        title: "상속자들", year: 2013,
+        image: "drama_img/the_heirs.jpg"
+    },
+    { 
+        text: "죽는 게 무서운 게 아니라, 잊히는 게 무섭다.", 
+        title: "슬기로운 의사생활", year: 2020,
+        image: "drama_img/hospital_playlist.jpg"
+    },
+    { 
+        text: "잊으려 하였으나 너를 잊지 못하였다.", 
+        title: "해를 품은 달", year: 2012,
+        image: "drama_img/moon_embracing_the_sun.jpg"
+    },
+    { 
+        text: "몰랐으니까.. 해방될 지 몰랐으니까!", 
+        title: "암살", year: 2015,
+        image: "drama_img/assassination.jpg"
+    }
 ];
